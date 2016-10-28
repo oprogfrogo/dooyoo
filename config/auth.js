8521a21ad945d886557da89721517edf40d7fe04
Simon Lee <slee@bslogin.com>
2016-10-28 09:31:00 -0700

    Auth

---
+++ config/auth.js
@@ -0,0 +1,17 @@
+module.exports = {  
+  facebookAuth: {
+    clientID: '1837823816494131',
+    clientSecret: 'ee8224123eec7c548cad9740c94271ad',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  },
+  twitterAuth: {
+    consumerKey: 'YOUR-TWITTER-CONSUMER-KEY',
+    consumerSecret: 'YOUR-TWITTER-CONSUMER-SECRET',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback',
+  },
+  googleAuth: {
+    clientID: 'YOUR-GOOGLE-CLIENT-ID',
+    clientSecret: 'YOUR-GOOGLE-CLIENT-SECRET',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+  },
+};
